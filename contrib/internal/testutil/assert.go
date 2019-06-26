4b57536aa71cbc3bf26a2e86faba0903f418c85e
Akash Suresh <akash@signalfx.com>
2019-06-26 18:40:24 -0400

    Deference pointer after nil check in assert.go

--- contrib/internal/testutil/assert.go
+++ contrib/internal/testutil/assert.go
@@ -19,13 +19,13 @@ func AssertSpanWithNoErrorEvent(t *testing.T, expected map[string]interface{}, a
 }
 
 func AssertSpan(t *testing.T, expected map[string]interface{}, actualSpan *trace.Span) {
-	assertIfNotNil(t, expected["kind"], *actualSpan.Kind)
-	assertIfNotNil(t, expected["name"], *actualSpan.Name)
+	assertIfNotNil(t, expected["kind"], actualSpan.Kind)
+	assertIfNotNil(t, expected["name"], actualSpan.Name)
 	assert.Equal(t, expected["tags"], actualSpan.Tags)
 }
 
-func assertIfNotNil(t *testing.T, expected interface{}, actual string) {
+func assertIfNotNil(t *testing.T, expected interface{}, actual *string) {
 	if assert.NotNil(t, actual) {
-		assert.Equal(t, expected, actual)
+		assert.Equal(t, expected, *actual)
 	}
 }
